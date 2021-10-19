69151d7a1cd8412a7dc2756754f37cc3df146670
Oleksandra Koshman <email [at] horiunova.sasha@gmail.com>
2021-10-19 19:19:27 +0300

    added sending user invitation

---
+++ IoTHomeAssistant.Domain/Dto/UserAddDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IoTHomeAssistant.Domain.Dto
+{
+    public class UserAddDto
+    {
+        [Required(ErrorMessage = "Це поле обов'язкове для заповнення!")]
+        public string Email { get; set; }
+    }
+}
