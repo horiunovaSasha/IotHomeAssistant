956e6a3f6afceeef16b0180b41ad73757eb7a9ca
Oleksandra Koshman <horiunova.sasha@gmail.com>
2021-10-26 13:38:46 +0300

    Added Xiaomi Temperature and Humidity Plugins

---
+++ Plugins/Xiaomi/Xiaomi.Temperature/MqttBackgroundService.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Extensions.Hosting;
+using MiHomeLib;
+using MiHomeLib.Devices;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace Xiaomi.Temperature
+{
+    public class MqttBackgroundService : IHostedService, IDisposable
+    {
+        private const string MQTT_CLIENT_ID = "Xiaomi.Temperature";
+
+        private readonly MqttClient _client;
+        private readonly MiHome _miHome;
+        private ThSensor _thSensor;
+
+        public MqttBackgroundService()
+        {
+            _client = new MqttClient(VariableExtension.MQTT_ADDR);
+            _miHome = new MiHome(VariableExtension.HUB_ID);
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _client.Connect(MQTT_CLIENT_ID + VariableExtension.DEVICE_ID);
+
+            _client.MqttMsgPublishReceived += async (object sender, MqttMsgPublishEventArgs e) =>
+            {
+                if (e.Topic == VariableExtension.STATUS_TOPIC)
+                {
+                    await SendStatus();
+                }
+            };
+
+            _client.Subscribe(new string[] { VariableExtension.STATUS_TOPIC }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+
+            _miHome.OnThSensor += (_, thSensor) =>
+            {
+                if (thSensor.Sid == VariableExtension.DEVICE_ID)
+                {
+                    if (_thSensor == null)
+                    {
+                        _thSensor = thSensor;
+                    }
+
+                    thSensor.OnTemperatureChange += (_, e) =>
+                    {
+                        _client.Publish(VariableExtension.SEND_STATUS_TOPIC, Encoding.UTF8.GetBytes(e.Temperature.ToString()));
+                    };
+                }
+            };
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _client.Disconnect();
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _client.Disconnect();
+        }
+
+        private async Task SendStatus(bool untilStop = false)
+        {
+            if (_thSensor != null)
+            {
+                _client.Publish(VariableExtension.SEND_STATUS_TOPIC, 
+                    Encoding.UTF8.GetBytes(_thSensor.Temperature.ToString()));
+            }
+        }
+    }
+}
---
+++ Plugins/Xiaomi/Xiaomi.Temperature/VariableExtension.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+
+namespace Xiaomi.Temperature
+{
+    static class VariableExtension
+    {
+        static public string STATUS_TOPIC { get; private set; }
+        static public string SEND_STATUS_TOPIC { get; private set; }
+        static public string MQTT_ADDR { get; private set; }
+        static public string MQTT_USR { get; private set; }
+        static public string MQTT_PWD { get; private set; }
+        static public string DEVICE_ID { get; private set; }
+        static public string HUB_ID { get; private set; }
+
+        static VariableExtension()
+        {
+            foreach (DictionaryEntry arg in Environment.GetEnvironmentVariables())
+            {
+                if (arg.Key.ToString() == "STATUS_TOPIC")
+                {
+                    STATUS_TOPIC = arg.Value?.ToString();
+                }
+
+                if (arg.Key.ToString() == "SEND_STATUS_TOPIC")
+                {
+                    SEND_STATUS_TOPIC = arg.Value?.ToString();
+                }
+
+                if (arg.Key.ToString() == "MQTT_ADDR")
+                {
+                    MQTT_ADDR = arg.Value?.ToString();
+                }
+
+                if (arg.Key.ToString() == "MQTT_USR")
+                {
+                    MQTT_USR = arg.Value?.ToString();
+                }
+
+                if (arg.Key.ToString() == "MQTT_PWD")
+                {
+                    MQTT_PWD = arg.Value?.ToString();
+                }
+
+                if (arg.Key.ToString() == "DEVICE_ID")
+                {
+                    DEVICE_ID = arg.Value?.ToString();
+                }
+
+                if (arg.Key.ToString() == "HUB_ID")
+                {
+                    HUB_ID = arg.Value?.ToString();
+                }
+            }
+
+            if (string.IsNullOrEmpty(MQTT_ADDR))
+            {
+                throw new ArgumentException("MQTT_ADDR Environment variable is required!");
+            }
+
+            if (string.IsNullOrEmpty(STATUS_TOPIC))
+            {
+                throw new ArgumentException("STATUS_TOPIC Environment variable is required!");
+            }
+
+            if (string.IsNullOrEmpty(SEND_STATUS_TOPIC))
+            {
+                throw new ArgumentException("SEND_STATUS_TOPIC Environment variable is required!");
+            }
+
+            if (string.IsNullOrEmpty(DEVICE_ID))
+            {
+                throw new ArgumentException("DEVICE_ID Environment variable is required!");
+            }
+
+            if (string.IsNullOrEmpty(HUB_ID))
+            {
+                throw new ArgumentException("HUB_ID Environment variable is required!");
+            }
+        }
+    }
+}
